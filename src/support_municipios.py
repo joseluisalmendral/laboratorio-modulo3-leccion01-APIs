a46b68b31f65ee61189ae73703a58a7470c4c9ee
joseluisalmendral <joseluisfunnels@gmail.com>
2024-10-15 19:05:50 +0200

    done a

--- src/support_municipios.py
+++ src/support_municipios.py
@@ -1,9 +1,13 @@
+import os
 import time
+from dotenv import load_dotenv
 import pandas as pd
 from tqdm import tqdm
 import geopy.geocoders
 from geopy.geocoders import Nominatim
 import requests
+
+
 
 geopy.geocoders.options.default_user_agent = 'nombre_app'
 geopy.geocoders.options.default_timeout = 7
@@ -30,7 +34,7 @@ def obtener_coordenadas(lista):
 
 
 
-def obtener_lugares(df_munis, cantidad):
+def obtener_lugares(df_munis, categorias, cantidad, token):
 
     municipios = df_munis[0:cantidad]
     
@@ -38,9 +42,30 @@ def obtener_lugares(df_munis, cantidad):
 
     headers = {
         "accept": "application/json",
-        "Authorization": "fsq38eO5vnBpB/6sfSyaptYChY624DcCYqh6DlLcSJCemy0="
+        "Authorization": f"{token}"
     }
 
     response = requests.get(url, headers=headers)
 
     print(response.text)
+
+
+
+
+def localizar(latlong, categoria, radio, maximo, ordenar, token):
+
+    url = f"https://api.foursquare.com/v3/places/search?ll={latlong}&radius={radio}&categories={categoria}&sort={ordenar.upper()}&limit={maximo}"
+    headers = {
+    "Accept": "application/json",
+    "Authorization": token
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
+categorias = {
+    16032 : 'Parques o areas verdes para rodajes exteriores',
+    17114 : 'Centros comerciales',
+    13065 : 'Bares o restaurantes',
+    17031 : 'Tiendas de disfraces',
+    11037 : 'Alquileres de equipos audiovisuales'
+}
